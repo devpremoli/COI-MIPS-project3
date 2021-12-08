a4bcc20a58a3c098e116e70d77d2089818003633
Prem20560404 <prem.oli@bison.howard.edu>
2021-12-08 00:54:51 -0500

    in checking branching to invalid if $t2 equals to 0, and in valid incrementing t3 register by 1, and multiplying the t7 register and t2 register and storing it to t2 register

--- project3.s
+++ project3.s
@@ -30,14 +30,15 @@ beq $s0, 44, substring
 j loop
 
 loop:
-lb $s0, ($t0) 								# loading bits
-beq $s0, 0, substring 							# undefined bits
+lb $s0, ($t0) 							
+beq $s0, 0, substring 					
 beq $s0, 10, substring
 addi $t0,$t0,1
 beq $s0, 44, substring
 
 
 checking:
+bgt $t2,0,invalid
 beq $s0, 9,  skipping 			
 beq $s0, 32, skipping
 ble $s0, 47, invalid 
@@ -52,6 +53,10 @@ bgt $s0, 113, invalid
 skipping:
 j loop
 
+valid:
+addi $t3, $t3,1
+mul $t2,$t2,$t7
+j loop
     
 
 removing:
