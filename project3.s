ada34b28d9445f0ab7561bd1d015ee18647ca495
Prem20560404 <prem.oli@bison.howard.edu>
2021-12-08 00:50:02 -0500

    In skipping label jumping to loop label, and in loop label, accessing another character, and looping to substring if character is null or end of line and comma.

--- project3.s
+++ project3.s
@@ -27,18 +27,31 @@ beq $s0, 32, removing
 beq $s0, 0, substring 		
 beq $s0, 10, substring
 beq $s0, 44, substring
+j loop
+
+loop:
+lb $s0, ($t0) 								# loading bits
+beq $s0, 0, substring 							# undefined bits
+beq $s0, 10, substring
+addi $t0,$t0,1
+beq $s0, 44, substring
 
 
 checking:
-beq $s0, 9,  skipping 							# if tab, move to gap function
+beq $s0, 9,  skipping 			
 beq $s0, 32, skipping
 ble $s0, 47, invalid 
 ble $s0, 57, valid 					
 ble $s0, 64, invalid
-ble $s0, 81, valid 						# starting with A (capital) = valid
-ble $s0, 96, invalid 						# less than 96 = invalid
-ble $s0, 113, valid 						# less than 117 (u) = valid
+ble $s0, 81, valid 						
+ble $s0, 96, invalid 						
+ble $s0, 113, valid
 bgt $s0, 113, invalid
+
+
+skipping:
+j loop
+
     
 
 removing:
